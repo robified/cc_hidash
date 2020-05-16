be94a04c297ccdf988cbeecd8f7bb9831ce67264
Robin <daorob@gmail.com>
2020-05-16 08:12:57 -0500

    Setup test to verify the dropdown state.

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,1 +1,18 @@
-it('works!!', () => {});
+// it('works!!', () => {});
+it('Shows an autocomplete', () => {
+    createAutoComplete({
+        root: document.querySelector('#target'),
+        fetchData() {
+            return [
+                { Title: 'Avengers' },
+                { Title: 'Not Avengers' },
+                { Title: 'Some other movie' },
+            ];
+        },
+        renderOption(movie) {
+            return movie.Title;
+        },
+    });
+
+    const dropdown = document.querySelector('.dropdown');
+});
