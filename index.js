aac37c83fab8cf81e3cf0de6103522716e0993d4
Robin <daorob@gmail.com>
2020-05-16 06:03:35 -0500

    Implement a test driven development, and pass successfully.

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ module.exports = {
             func(array[index], index);
         }
     },
+    map(array, func) {
+        const result = [];
+        for (let i = 0; i < array.length; i++) {
+            result.push(func(array[i], i));
+        }
+        return result;
+    },
 };
 
 /* How we would manually test this function we just wrote
 forEach([1, 2, 3], (value) => {
     console.log(value);
 });
+
+map([1,2,3], asdfjkl;)
+console.log(blah blah blah)
 */
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const { forEach } = require('./index');
-
+const { forEach, map } = require('./index');
+// the test for forEach
 let sum = 0;
 forEach([1, 2, 3], (value) => {
     sum += value;
@@ -9,4 +9,19 @@ forEach([1, 2, 3], (value) => {
 // doing it this way is better than the console.log(sum) way
 if (sum !== 6) {
     throw new Error('Expected summing array to equal 6');
+}
+
+// the test for map
+const result = map([1, 2, 3], (value) => {
+    return value * 2;
+});
+// result === [2, 4, 6]
+if (result[0] !== 2) {
+    throw new Error(`Expected to find 2, but found {result[0]}`);
+}
+if (result[1] !== 4) {
+    throw new Error(`Expected to find 4, but found {result[0]}`);
+}
+if (result[2] !== 6) {
+    throw new Error(`Expected to find 6, but found {result[0]}`);
 }
