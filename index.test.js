8704f0eff751b4226f178d8b56c03e55eae9a0d6
Robin <daorob@gmail.com>
2020-05-16 06:27:40 -0500

    Fix issue #1) variable scopes collision, #2) continue running test after an error, and #3) add description for each test.

--- index.test.js
+++ index.test.js
@@ -1,27 +1,44 @@
 const { forEach, map } = require('./index');
-// the test for forEach
-let sum = 0;
-forEach([1, 2, 3], (value) => {
-    sum += value;
+
+const test = (description, func) => {
+    console.log('----', description);
+    try {
+        func();
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+test('The forEach function', () => {
+    // the test for forEach
+    console.log('Test for forEach');
+    let sum = 0;
+    forEach([1, 2, 3], (value) => {
+        sum += value;
+    });
+
+    // console.log(sum) to manually see the results in the terminal
+    // doing it this way is better than the console.log(sum) way
+    if (sum !== 6) {
+        throw new Error('Expected summing array to equal 6');
+    }
+    console.log("DIDN'T GET HERE!!!");
 });
 
-// console.log(sum) to manually see the results in the terminal
-// doing it this way is better than the console.log(sum) way
-if (sum !== 6) {
-    throw new Error('Expected summing array to equal 6');
-}
-
-// the test for map
-const result = map([1, 2, 3], (value) => {
-    return value * 2;
+test('The mapfunction', () => {
+    // the test for map
+    console.log('Test for map');
+    const result = map([1, 2, 3], (value) => {
+        return value * 2;
+    });
+    // result === [2, 4, 6]
+    if (result[0] !== 2) {
+        throw new Error(`Expected to find 2, but found {result[0]}`);
+    }
+    if (result[1] !== 4) {
+        throw new Error(`Expected to find 4, but found {result[0]}`);
+    }
+    if (result[2] !== 6) {
+        throw new Error(`Expected to find 6, but found {result[0]}`);
+    }
 });
-// result === [2, 4, 6]
-if (result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found {result[0]}`);
-}
-if (result[1] !== 4) {
-    throw new Error(`Expected to find 4, but found {result[0]}`);
-}
-if (result[2] !== 6) {
-    throw new Error(`Expected to find 6, but found {result[0]}`);
-}
