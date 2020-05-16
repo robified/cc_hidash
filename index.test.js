a2ddfe527cab2fb8e73e78bd81c88372d2d1f077
Robin <daorob@gmail.com>
2020-05-16 06:40:31 -0500

    Implement the assert module with test cases passing successfully.

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
 const test = (description, func) => {
@@ -19,10 +20,13 @@ test('The forEach function', () => {
 
     // console.log(sum) to manually see the results in the terminal
     // doing it this way is better than the console.log(sum) way
-    if (sum !== 6) {
-        throw new Error('Expected summing array to equal 6');
-    }
-    console.log("DIDN'T GET HERE!!!");
+    // if (sum !== 6) {
+    //     throw new Error('Expected summing array to equal 6');
+    // }
+    // console.log("DIDN'T GET HERE!!!");
+
+    // this replace the if statement above
+    assert.strictEqual(sum, 7, 'Expected forEach to sum the array');
 });
 
 test('The mapfunction', () => {
@@ -32,13 +36,21 @@ test('The mapfunction', () => {
         return value * 2;
     });
     // result === [2, 4, 6]
-    if (result[0] !== 2) {
-        throw new Error(`Expected to find 2, but found {result[0]}`);
-    }
-    if (result[1] !== 4) {
-        throw new Error(`Expected to find 4, but found {result[0]}`);
-    }
-    if (result[2] !== 6) {
-        throw new Error(`Expected to find 6, but found {result[0]}`);
-    }
+
+    // if (result[0] !== 2) {
+    //     throw new Error(`Expected to find 2, but found {result[0]}`);
+    // }
+    // assert.strictEqual(result[0], 2);
+
+    // if (result[1] !== 4) {
+    //     throw new Error(`Expected to find 4, but found {result[0]}`);
+    // }
+    // assert.strictEqual(result[1], 4);
+
+    // if (result[2] !== 6) {
+    //     throw new Error(`Expected to find 6, but found {result[0]}`);
+    // }
+    // assert.strictEqual(result[2], 6);
+
+    assert.deepStrictEqual(result, [2, 4, 6]);
 });
